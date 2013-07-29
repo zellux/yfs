82e0bb8cba7b6da5b05099b695f392b843f0426b
Yuanxuan Wang <zellux@gmail.com>
2013-07-29 08:24:26 +0800

    Extent server operations

--- extent_server.cc
+++ extent_server.cc
@@ -1,42 +1,91 @@
 // the extent server implementation
 
 #include "extent_server.h"
-#include <sstream>
+#include <fstream>
+#include <utility>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
+// TODO make all operations atomic
+
 extent_server::extent_server() {}
 
-int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &)
+int
+extent_server::put(extent_protocol::extentid_t id, std::string buf, int &)
 {
-    // You fill this in for Lab 2.
-    return extent_protocol::IOERR;
+    extent_protocol::attr attr;
+    std::ifstream is(local_path(id).c_str(), std::ios::binary);
+    if (is.is_open()) {
+        is >> attr.size >> attr.atime >> attr.mtime >> attr.ctime;
+    } else {
+        time_t current = time(NULL);
+        attr.size = buf.size();
+        attr.atime = 0;
+        attr.mtime = current;
+        attr.ctime = current;
+    }
+    is.close();
+
+    std::ofstream os(local_path(id).c_str(), std::ios::binary);
+    os << attr.size << attr.atime <<  attr.mtime << attr.ctime;
+    os << buf;
+    os.close();
+
+    truncate(local_path(id).c_str(), buf.size() + HEADER_SIZE);
+    
+    return extent_protocol::OK;
 }
 
-int extent_server::get(extent_protocol::extentid_t id, std::string &buf)
+int
+extent_server::get(extent_protocol::extentid_t id, std::string &buf)
 {
-    // You fill this in for Lab 2.
-    return extent_protocol::IOERR;
+    extent_protocol::attr attr;
+    std::ifstream is(local_path(id).c_str(), std::ios::binary);
+    if (is.is_open())
+        return extent_protocol::NOENT;
+        
+    is >> attr.size >> attr.atime >> attr.mtime >> attr.ctime;
+    buf.resize(attr.size);
+    is.read(&buf[0], attr.size);
+    is.close();
+    return extent_protocol::OK;
 }
 
-int extent_server::getattr(extent_protocol::extentid_t id, extent_protocol::attr &a)
+int
+extent_server::getattr(extent_protocol::extentid_t id, extent_protocol::attr &a)
 {
     // You fill this in for Lab 2.
     // You replace this with a real implementation. We send a phony response
     // for now because it's difficult to get FUSE to do anything (including
     // unmount) if getattr fails.
-    a.size = 0;
-    a.atime = 0;
-    a.mtime = 0;
-    a.ctime = 0;
+    std::ifstream is(local_path(id).c_str(), std::ios::binary);
+    if (is.is_open())
+        return extent_protocol::NOENT;
+    is >> a.size >> a.atime >> a.mtime >> a.ctime;
+    is.close();
+    
+    a.atime = time(NULL);
+    std::ofstream os(local_path(id).c_str(), std::ios::binary);
+    os.seekp(0);
+    os << a.size << a.atime <<  a.mtime << a.ctime;
+    os.close();
+    
     return extent_protocol::OK;
 }
 
-int extent_server::remove(extent_protocol::extentid_t id, int &)
+int
+extent_server::remove(extent_protocol::extentid_t id, int &)
 {
-    // You fill this in for Lab 2.
-    return extent_protocol::IOERR;
+    std::ifstream is(local_path(id).c_str());
+    if (is.is_open())
+        return extent_protocol::NOENT;
+    is.close();
+
+    if (::remove(local_path(id).c_str()))
+        return extent_protocol::IOERR;
+    else
+        return extent_protocol::OK;
 }
--- extent_server.h
+++ extent_server.h
@@ -5,8 +5,10 @@
 
 #include <string>
 #include <map>
+#include <sstream>
 #include "extent_protocol.h"
 
+#define EXTENT_SERVER_ROOT "objects/"
 class extent_server {
 
  public:
@@ -16,6 +18,21 @@ class extent_server {
     int get(extent_protocol::extentid_t id, std::string &);
     int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
     int remove(extent_protocol::extentid_t id, int &);
+
+ private:
+    inline const std::string local_path(extent_protocol::extentid_t id);
+    static const int HEADER_SIZE = sizeof(((extent_protocol::attr *) 0)->size) +
+        sizeof(((extent_protocol::attr *) 0)->atime) +
+        sizeof(((extent_protocol::attr *) 0)->mtime) +
+        sizeof(((extent_protocol::attr *) 0)->ctime);
 };
 
-#endif 
+inline const std::string
+extent_server::local_path(extent_protocol::extentid_t id) {
+    std::ostringstream ost;
+    ost << EXTENT_SERVER_ROOT;
+    ost << id;
+    return ost.str();
+}
+
+#endif
